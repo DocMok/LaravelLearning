cf29b748e53a24117552aec4f0279f8def3baef6
DocMok <aleksandr.arendarenko@gmail.com>
2021-05-25 16:44:44 +0300

    problem into blades

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,3 +1,4 @@
+@props(['post'])
 <article
     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5">
@@ -8,12 +9,9 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="#"
+                    <a href="/categories/{{$post->category->slug}}"
                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                       style="font-size: 10px">Techniques</a>
-                    <a href="#"
-                       class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                       style="font-size: 10px">Updates</a>
+                       style="font-size: 10px">hjdfhj</a>
                 </div>
 
                 <div class="mt-4">
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -17,18 +17,19 @@
 
     @include('_posts-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        <x-post-featured-card :post="$posts[0]" />
 
         <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
+            @foreach ($posts->skip(1) as $post)
+                <x-post-card :post="$post"/>
+            @endforeach
         </div>
 
-        <div class="lg:grid lg:grid-cols-3">
+<!--         <div class="lg:grid lg:grid-cols-3">
             <x-post-card />
             <x-post-card />
             <x-post-card />
         </div>
-    </main>
+ -->    </main>
 
 </x-layout>
