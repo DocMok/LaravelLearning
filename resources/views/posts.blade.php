a64d51dcb088ef3b10d4da54387cbae454f8cbc6
DocMok <aleksandr.arendarenko@gmail.com>
2021-05-26 08:52:30 +0300

    fixed bug in blades

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,23 @@
 <x-layout>
-{{--    @foreach ($posts as $post)--}}
-{{--        <article>--}}
-{{--            <h1>--}}
-{{--                <a href="/posts/{{ $post->slug }}">--}}
-{{--                    {!!  $post->title !!}--}}
-{{--                </a>--}}
-{{--            </h1>--}}
+    {{--    @foreach ($posts as $post)--}}
+    {{--        <article>--}}
+    {{--            <h1>--}}
+    {{--                <a href="/posts/{{ $post->slug }}">--}}
+    {{--                    {!!  $post->title !!}--}}
+    {{--                </a>--}}
+    {{--            </h1>--}}
 
-{{--            <p>--}}
-{{--                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>--}}
-{{--            </p>--}}
+    {{--            <p>--}}
+    {{--                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>--}}
+    {{--            </p>--}}
 
-{{--            <div>{{ $post->excerpt }}</div>--}}
-{{--        </article>--}}
-{{--    @endforeach--}}
+    {{--            <div>{{ $post->excerpt }}</div>--}}
+    {{--        </article>--}}
+    {{--    @endforeach--}}
 
     @include('_posts-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post="$posts[0]" />
+        <x-post-featured-card :post="$posts[0]"/>
 
         <div class="lg:grid lg:grid-cols-2">
             @foreach ($posts->skip(1) as $post)
@@ -25,11 +25,11 @@
             @endforeach
         </div>
 
-<!--         <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
- -->    </main>
+{{--        <div class="lg:grid lg:grid-cols-3">--}}
+{{--            <x-post-card/>--}}
+{{--            <x-post-card/>--}}
+{{--            <x-post-card/>--}}
+{{--        </div>--}}
+    </main>
 
 </x-layout>
