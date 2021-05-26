319161b909a21228c2321dd89b5446f638e693c7
DocMok <aleksandr.arendarenko@gmail.com>
2021-05-26 17:06:15 +0300

    Some changes in blades

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
             'name'=>'DocMok',
             'username'=>'docmok'
         ]);
-        Post::factory(5)->create([
+        Post::factory(10)->create([
             'user_id'=>$user->id
         ]);
     }
